7a76a6181da226c5877fbe8f8204351eb8a40f37
Khalid Williams <khalidwill27@gmail.com>
2019-09-08 17:29:52 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  // CSS and file (image) loaders
+  module: {
+  },
+  // Below is needed for webpack-dev-server
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
+};
