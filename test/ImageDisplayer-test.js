e38cc35c2f503840d0d4b5eec3373d297b9fbdb2
Khalid Williams <khalidwill27@gmail.com>
2019-09-10 10:13:45 -0600

    Finish spy test

--- test/ImageDisplayer-test.js
+++ test/ImageDisplayer-test.js
@@ -1,6 +1,5 @@
 import mocha from 'mocha';
 import chai, { expect } from 'chai';
-// expect = chai.expect;
 import spies from 'chai-spies';
 import ImageDisplayer from '../src/ImageDisplayer';
 
@@ -8,34 +7,27 @@ chai.use(spies);
 
 describe('ImageDisplayer', () => {
   let imageDisplayer;
+
   before(() => {
     imageDisplayer = new ImageDisplayer("https://api.nasa.gov/planetary/apod");
-    global.fetch = () => new Promise((resolve, reject) => {resolve({}), reject("Error")});
-  })
+    global.fetch = () => {};
+  });
 
   it('should return true', () => {
     expect(true).to.equal(true);
-  })
+  });
 
   it('should return an ImageDisplayer', () => {
     expect(imageDisplayer).to.be.an.instanceof(ImageDisplayer);
-  })
+  });
 
   it('should take a root', () => {
     expect(imageDisplayer.root).to.equal("https://api.nasa.gov/planetary/apod");
-  })
+  });
 
-  it('should convert a query objects to a query string', () => {
-    const queryString = imageDisplayer.convertQueryToString({hello: "world", type: "test"})
-    expect(queryString).to.equal("?hello=world&type=test");
-  })
-
-  // it('should get today\'s space picture by default', () => {
-  //   const picOfToday = imageDisplayer.getPicture();
-  //   expect(picOfToday).to.exist;
-  // })
-  it('should get today\'s space picture by default', () => {
-    const picOfToday = imageDisplayer.getPicture();
-    chai.spy.on(fetch);
-  })
-})
+  it('should get a response to a request when created', () => {
+    chai.spy.on(global, "fetch")
+    imageDisplayer.getPicture(imageDisplayer.root);
+    expect(global.fetch).to.be.called();
+  });
+});
