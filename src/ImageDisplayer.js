03f7a784290079424e69e98968c18f3e7cfea5d1
Khalid Williams <khalidwill27@gmail.com>
2019-09-10 10:37:07 -0600

    Clean up ImageDisplayer class

--- src/ImageDisplayer.js
+++ src/ImageDisplayer.js
@@ -5,26 +5,9 @@ class ImageDisplayer {
   }
 
   getPicture() {
-    // const queryString = this.convertQueryToString(queryInfo);
-    // let url = this.root + queryString;
-
-    return fetch(this.root)
-      // .then(data => data.json())
-      // .then(data => this.response = data)
-      // .catch(err => console.log(err));
+    return fetch(this.root);
   }
 
-  // convertQueryToString(queryInfo) {
-  //   if(queryInfo) {
-  //     let queries = Object.keys(queryInfo);
-  //     return queries.reduce((acc, key, i) => {
-  //       let and = (i < queries.length - 1) ? "&" : "";
-  //       acc += `${key}=${queryInfo[key]}${and}`;
-  //       return acc;
-  //     }, "?")
-  //   }
-  //   return "";
-  // }
 
 
 };
